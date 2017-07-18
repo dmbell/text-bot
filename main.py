1720456a0bd0454742b55ffc8db4b8fd611d4dab
dmbell <dougmbell@gmail.com>
2017-07-18 10:23:34 -0400

    Add bot login/stream submission monitoring. Texting buggy (Twilio issue)

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
+import sys
+import praw
+import datetime
 import config
 from twilio.rest import Client
 
-client = Client(config.twilio_sid, config.twilio_auth)
+def main():
+	reddit = bot_login()
+	subreddit = get_subreddit(reddit)
+	start_time = datetime.datetime.today()
+	print("Start time: " + str(start_time))
+	monitor_sub(subreddit, start_time)
 
-message = client.messages.create(body=
-	"Mom help it's me Crowley I'm stuck under the bed. Bring treats.",
-	from_=config.twilio_cell, to=config.target_cell)
+def bot_login():
+	reddit = praw.Reddit(username = config.username,
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = "MK Buying Notification v1.0")
+	return reddit
+
+def get_subreddit(reddit):
+	return reddit.subreddit(sys.argv[1])
+
+def monitor_sub(subreddit, start_time):
+	for submission in subreddit.stream.submissions():
+		post_time = datetime.datetime.fromtimestamp(submission.created_utc)
+		print(submission.title + " --- " + str(post_time))
+		if post_time > start_time:
+			print("Texting...")
+			notify(submission.title + " --- " + submission.shortlink)
+
+def notify(message):
+	client = Client(config.twilio_sid, config.twilio_auth)
+	client.messages.create(body=message, from_=config.twilio_cell, to=config.target_cell)
+
+
+if __name__ == "__main__":
+	main()
